85c0a1abda65451cc4d53c3d2808ccbf4c100d1d
Anoop Tatti <anoop@live.co.uk>
2019-11-30 13:02:09 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
-#!/bin/sh -l
+#!/bin/bash
+set -e
+set -o pipefail
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+if [[ -z "$ADMIN_USERNAME" ]]; then
+  echo "Admin user name - ADMIN_USERNAME - not set."
+  exit 1
+fi
+
+if [[ -z "$ADMIN_PASSWORD" ]]; then
+  echo "Admin password - ADMIN_PASSWORD - not set."
+  exit 1
+fi
+
+
+main() {
+    echo "Logging into tenant using O365 CLI..."
+    o365 login --authType password --userName $ADMIN_USERNAME --password $ADMIN_PASSWORD
+    o365 status  
+    echo "Logged in."
+}
+
+main "$@"
